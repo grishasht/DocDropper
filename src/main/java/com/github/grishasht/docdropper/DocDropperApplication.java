72614f3ccdd76ab35c26bad798fcf7d8bbc432c7
grishasht <grishasht@bigmir.net>
2020-05-25 01:55:21 +0300

    Init commit

---
+++ src/main/java/com/github/grishasht/docdropper/DocDropperApplication.java
@@ -0,0 +1,15 @@
+package com.github.grishasht.docdropper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocDropperApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DocDropperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/grishasht/docdropper/DocDropperApplicationTests.java
@@ -0,0 +1,15 @@
+package com.github.grishasht.docdropper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocDropperApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
